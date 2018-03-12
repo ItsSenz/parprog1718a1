c330b6690b0a2c785bff3892be2661242258f31d
ItsSenz <p14kats@ionio.gr>
2018-03-12 12:22:58 +0200

    Update matrix1.c

--- matrix1.c
+++ matrix1.c
@@ -3,6 +3,7 @@
 #include <sys/time.h>
 
 // compile like:  gcc -Wall -O2 -DNROWS=10000 matrix1.c -o matrix1
+// prospelash ana sthlh tou pinaka
 
 
 #define NCOLS 100
@@ -15,8 +16,10 @@ void get_walltime(double *wct) {
 
 
 int main() {
+FILE *pFile;
 double *table;
 double ts,te;
+pFile=fopen("results.csv", "a");
 
 
   table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
@@ -27,25 +30,42 @@ double ts,te;
 
   // warmup
 
-  // ...your code here...
+  for (i = 0; i <NROWS* NCOLS; i++){
+	table[i] = 5.0; 
+  }
 
   // get starting time (double, seconds) 
   get_walltime(&ts);
   
   // workload
 
-  // ...your code here...
+  for(i=0;i < NROWS;i++){
+	for(j=0;j < NCOLS; j++){			
+		table[j*NROWS + i] = table[j*NROWS + i] + 2.0;
+
+	}
+}
 
   // get ending time
   get_walltime(&te);
 
   // check results
   
-  // ...your code here...
+  for (i = 0; i <NROWS* NCOLS; i++){
+	  if(table[i] != 7.0){	
+		  printf ("Wups something went wrong ... Check your code..\n");
+		  break;
+	  }
+  }
 
   // print time elapsed and/or Maccesses/sec
   
-  // ...your code here...  
+  double time = te - ts;
+          
+	printf("ok\n time took = %lf\n",time);
+	printf("Megaccess/sec = %lf\n",(2.0 * NROWS*NCOLS)/(time*1e6));
+	fprintf(pFile, "%lf \n",time);
+	fprintf(pFile, "%lf \n",(2.0*NROWS*NCOLS)/(time*1e6)); 
   
   free(table);
 
